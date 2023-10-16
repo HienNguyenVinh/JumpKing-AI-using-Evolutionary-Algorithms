ff32a58fc1f8fc87afeb25fb3cc81fac9a68ba48
Nguyen Vinh Hien <126568387+HienNguyenVinh@users.noreply.github.com>
2023-10-16 21:38:00 +0700

    Update population.py

--- subclass/population.py
+++ subclass/population.py
@@ -17,13 +17,20 @@ class Population:
         self.reached_best_level_at_action_no = 0
         self.clone_of_best_player_from_pre_generation = None
 
+        self.showing_fail = False
+        self.fail_player_no = 0
+
     def resetAllPlayers(self):
         for player in self.players:
             player.resetPlayer()
 
     def Update(self):
         for i in range(self.size):
-            self.players[i].Update()
+            self.players[i].Update(False)
+            if self.players[i].currentLevelNo > 0 and not self.showing_fail:
+                self.showing_fail = True
+                self.fail_player_no = i
+                self.resetAllPlayers()
 
     def setBestPlayer(self):
         self.best_player = self.players[0]
@@ -37,6 +44,7 @@ class Population:
             self.current_best_level_reached = self.best_player.best_level_reached
             self.new_level_reached = True
             self.reached_best_level_at_action_no = self.best_player.best_level_reached_on_action_no
+            print(f'New level, action number: {self.reached_best_level_at_action_no}')
 
         self.best_height = self.best_player.best_height_reached
 
@@ -52,6 +60,8 @@ class Population:
         self.fitness_sum = 0
         for player in self.players:
             player.calcFitness()
+            if player.best_level_reached < self.best_player.best_level_reached:
+                player.fitness = 0
             self.fitness_sum += player.fitness
 
     def selectParent(self):
@@ -73,16 +83,24 @@ class Population:
         self.clone_of_best_player_from_pre_generation = self.best_player.clone()
         next_gen.append(self.best_player.clone())
 
-        for i in range(1, self.size):
+        for i in range(1, len(self.players)):
             parent = self.selectParent()
             baby = parent.clone()
+
+            # if the parent fell to the previous level then mutate the baby at the action that caused them to fall
             if parent.felt_to_previous_level:
                 baby.brain.mutate_action_number(parent.felt_on_action_no)
 
             baby.brain.mutate()
             next_gen.append(baby)
 
-        self.players = next_gen
+        self.players = []
+        for i in range(len(next_gen)):
+            self.players.append(next_gen[i])
+
+            if not self.new_level_reached and self.current_best_level_reached != 0:
+                self.players[i].loadStartOfBestLevelPlayerState()
+
         self.gen += 1
 
     def increase_player_moves(self, increase_by):
@@ -110,4 +128,5 @@ class Population:
             # if player.currentLevelNo >= highest_level_no - 1 and player.currentLevelNo <= highest_level_no:
             #     player.Draw(window)
             if player.currentLevelNo == highest_level_no:
-                player.Draw(window)
+                player.alreadyShowingSnow = False
+                player.Draw(window, False)
