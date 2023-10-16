2e339123daf3cf95e9b6adeb6f22030eee7db65c
Nguyen Vinh Hien <126568387+HienNguyenVinh@users.noreply.github.com>
2023-10-16 21:37:20 +0700

    Update line.py

--- subclass/line.py
+++ subclass/line.py
@@ -3,6 +3,14 @@ import pygame
 
 Point = namedtuple("Point", ["x", "y"])
 
+def AreLinesColliding(a1, b1, a2, b2):
+    uA = ((b2[0] - a2[0]) * (a1[1] - a2[1]) - (b2[1] - a2[1]) * (a1[0] - a2[0])) / ((b2[1] - a2[1]) * (b1[0] - a1[0]) - (b2[0] - a2[0]) * (b1[1] - a1[1]))
+    uB = ((b1[0] - a1[0]) * (a1[1] - a2[1]) - (b1[1] - a1[1]) * (a1[0] - a2[0])) / ((b2[1] - a2[1]) * (b1[0] - a1[0]) - (b2[0] - a2[0]) * (b1[1] - a1[1]))
+    if(uA >= 0 and uA <= 1 and uB >= 0 and uB <= 1):
+        intersectionX = a1[0] + (uA * (b1[0] - a1[0]))
+        intersectionY = a1[1] + (uA * (b1[1] - a1[1]))
+        return [True, intersectionX, intersectionY]
+    return [False, 0, 0]
 
 class DiagonalCollisionInfo:
     def __init__(self):
@@ -38,6 +46,7 @@ class Line:
     def GetType(self):
         return (self.x1, self.y1, self.x2, self.y2)
 
+    # make sure that p1 is always in the lower left corner and p2 is always in the upper right corner of the line.
     def EnsureCoordnates(self):
         if not self.isDiagonal:
             if self.x1 > self.x2 or self.y1 > self.y2:
